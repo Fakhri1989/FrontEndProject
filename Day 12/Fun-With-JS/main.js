ba1ec73b9cf484a935fa99dcf2bf30de0551ed5d
Fakhri <fakhri.said1989@gmail.com>
2021-05-23 15:57:51 +0300

    done without the bonus.... yet

--- Day 12/Fun-With-JS/main.js
+++ Day 12/Fun-With-JS/main.js
@@ -1,6 +1,21 @@
-console.log("hello world");
+let o1 = {};
 
-let i = 20;
-let j = 35;
+let a1 = [2, 4, 6];
 
-console.log(i + " + " + j + " = " + (i+j));
+function Fakhri() {
+
+}
+
+var k1 = new Fakhri();
+var k2 = new Fakhri();
+
+k1.x = 20;
+k2.x = 40;
+
+Fakhri.prototype.z = 50;
+
+console.log(k1.z);
+console.log(k2.z);
+
+console.log(k1.x);
+console.log(k2.x);
---
+++ Day 12/ex1-js/main.js
@@ -0,0 +1,31 @@
+function Calculator(a, b){
+    this.number1 = a;
+    this.number2 = b;
+}
+
+Calculator.prototype.add = function (){
+    return (this.number1 + this.number2);
+}
+
+Calculator.prototype.substract = function (){
+    return (this.number1 - this.number2);
+}
+
+Calculator.prototype.multiply = function (){
+    return (this.number1 * this.number2);
+}
+
+Calculator.prototype.devide = function (){
+    return (this.number1 / this.number2);
+}
+
+Calculator.prototype.log = function (){
+    console.log(`the sum of the numbers is: ${this.add()} the difference of the numbers is ${this.substract()} and finally the product of the two numbers is ${this.multiply()}`);
+}
+
+var a = new Calculator(5, 10);
+var b = new Calculator(10, 5);
+
+
+a.log();
+b.log();
